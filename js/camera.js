1ab71898e3491150a9f0f2a4efd52dc2ce11a0ab
Luciano Silva <luciano_jet@hotmail.com>
2023-11-17 12:13:53 -0300

    ajustando para comecar

--- js/camera.js
+++ js/camera.js
@@ -1,40 +1,15 @@
-// function handleFileSelect(evt) {
-//     const file = evt.target.files[0]; 
-//     const reader = new FileReader();
-
-//     reader.onload = function (e) {
-
-//       const imgElement = document.getElementById('photo');
-//       imgElement.src = e.target.result; 
-//       imgElement.style.display = 'block'; 
-//     };
-
-//        reader.readAsDataURL(file);
-//   }
-
-
-//   document.getElementById('captureButton').addEventListener('click', function () {
-
-//     const fileInput = document.createElement('input');
-//     fileInput.type = 'file';
-//     fileInput.accept = 'image/*'; 
-
-//     fileInput.addEventListener('change', handleFileSelect);
-
-//     fileInput.click();
-//   });
-
-
-
-// ----------------------------SEGUNDO CÓDIGO
+import { app } from "./config-firebase.js";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-storage.js";
+import { collection, addDoc } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-firestore.js";
 
 let btnAvancar = document.querySelector("#btnAvancar");
 let confir = document.querySelector(".confirm");
 let imageContainer = document.getElementById("imageContainer");
 let confirmLabel = document.querySelector(".confirm-label");
 
-
 let images = [];
+let temporaryImageName = "";
+let temporaryImageStorage = [];
 
 document.getElementById('captureButton').addEventListener('click', function () {
   const fileInput = document.createElement('input');
@@ -46,7 +21,6 @@ document.getElementById('captureButton').addEventListener('click', function () {
 
   fileInput.click();
 });
-
 
 confir.addEventListener("change", function () {
   if (confir.checked) {
@@ -69,7 +43,7 @@ function handleFileSelect(evt) {
       const imgElement = document.createElement('img');
       imgElement.src = e.target.result;
       imgElement.alt = 'Imagem';
-      images.push(imgElement);
+      images.push({ file, imgElement });
       displayImages();
     };
 
@@ -83,7 +57,7 @@ function displayImages() {
   }
 
   for (let i = 0; i < images.length; i++) {
-    imageContainer.appendChild(images[i]);
+    imageContainer.appendChild(images[i].imgElement);
   }
   if (images.length > 0 && confir.checked) {
     btnAvancar.disabled = false;
@@ -92,16 +66,63 @@ function displayImages() {
   }
 }
 
-btnAvancar.addEventListener("click", (evento) => {
+btnAvancar.addEventListener("click", async (evento) => {
   evento.preventDefault();
 
   if (!confir.checked) {
-    alert("Você deve confirmar antes de prosseguir.");
+    alert("VOCÊ DEVE CONFIRMAR ANTES DE PROSSEGUIR.");
   } else if (images.length === 0) {
-    alert("Você deve inserir pelo menos uma imagem para prosseguir.");
+    alert("VOCÊ DEVE INSERIR PELO MENOS UMA IMAGEM PARA PROSSEGUIR.");
   } else {
-    const nextPageHref = btnAvancar.getAttribute("href");
-    window.location.href = nextPageHref;
+    // Armazenar temporariamente as imagens
+    temporaryImageStorage.push({ temporaryImageName, images });
+
+    // Limpar as imagens para a próxima página
+    images = [];
+    displayImages();
+
+    // Verificar se é a sexta página (proteger6.html)
+    if (window.location.href.includes("proteger6.html")) {
+      try {
+        const storage = getStorage(app);
+        const storageRef = ref(storage, 'gs://dplconstrucao-cfd1a.appspot.com'); // Referência ao storage principal
+
+        for (let i = 0; i < temporaryImageStorage.length; i++) {
+          const { temporaryImageName, images } = temporaryImageStorage[i];
+          const folderRef = ref(storageRef, `${temporaryImageName}`); // Referência à pasta com nome da página
+
+          for (let j = 0; j < images.length; j++) {
+            const { file } = images[j];
+            const fileRef = ref(folderRef, `${temporaryImageName}_${j}_${file.name}`);
+            await uploadBytes(fileRef, file);
+            const downloadURL = await getDownloadURL(fileRef);
+
+            // Agora você pode salvar o downloadURL no Firestore se necessário
+            // Exemplo:
+            // const docRef = await addDoc(collection(db, "imagens"), { downloadURL });
+            // console.log("Imagem enviada para o Firebase:", downloadURL);
+          }
+        }
+
+        const confirmEnvio = confirm("DESEJA REALMENTE ENVIAR O REGISTRO?");
+        if (confirmEnvio) {
+          alert("ENVIO BEM-SUCEDIDO! REDIRECIONANDO PARA A PÁGINA INICIAL!");
+          // Redirecionar para a página inicial após o envio bem-sucedido
+          window.location.href = "../index.html";
+        }
+      } catch (error) {
+        console.error("Erro ao enviar imagem para o Firebase:", error);
+      }
+    } else {
+      // Redirecionar para a próxima página após o envio bem-sucedido
+      const nextPageHref = btnAvancar.getAttribute("href");
+      window.location.href = nextPageHref;
+    }
   }
 });
 
+// Este código deve ser colocado em cada página para obter o nome da imagem temporária da tag h4
+const h4Element = document.querySelector("h4");
+if (h4Element) {
+  temporaryImageName = h4Element.innerText;
+}
